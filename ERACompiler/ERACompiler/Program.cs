b763a527c37e26c8cf19ce56e256ed6dbab124e4
Egor Klementev <egorklementeff@yandex.ru>
2020-04-19 14:39:05 +0300

    FILE ADDITION: gitignore
    Problem
    VS temporary files should not be commited.
    
    Solution
    Addition of the .gitignore file.
    
    Note

---
+++ ERACompiler/ERACompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ERACompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
